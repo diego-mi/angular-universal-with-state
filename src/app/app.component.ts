e79e33428dfcdb90278bcf2c2d3677ba82086d70
Diego Campos <diego.campos@zarpsystem.com.br>
2017-12-01 16:06:14 -0200

    Adicionado consumo de endpoint para teste

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'app';
+
+  dogs: any;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  ngOnInit() {
+    this.http
+      .get('https://dog.ceo/api/breeds/list/all')
+      .subscribe(data => {
+        console.log(data);
+        this.dogs = data;
+      });
+  }
 }
